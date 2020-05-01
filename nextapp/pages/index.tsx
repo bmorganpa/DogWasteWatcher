73ce6fdfb6186cf68fe7016ca8f1e9f77af3914c
Brad Morgan <brad.morgan@vistarmedia.com>
2020-05-01 12:56:05 -0400

    Added ESLint to CI
    * Fixed ESLint errors/warnings
    * Removed loading spinner from index

--- nextapp/pages/index.tsx
+++ nextapp/pages/index.tsx
@@ -1,38 +1,12 @@
 import React from "react";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
-import Link from "next/link";
-import { useTranslation } from "react-i18next";
 
-import { useUser } from "./_app";
 import { withApollo } from "../apollo/client";
 import { WasteMap } from "../components/index/WasteMap";
-import { PageWrapper } from "../components/PageWrapper";
 import { MapPane, MapControl } from "../components/MapPane";
 
 const Index = () => {
-  const { t } = useTranslation("index");
-
-  const { loading: userLoading, user } = useUser();
-
-  if (userLoading) {
-    return (
-      <div className="container">
-        <CircularProgress />
-        <style jsx>{`
-          .container {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-        `}</style>
-      </div>
-    );
-  }
-
   return (
     <WasteMap>
       <MapPane position="bottomright">
