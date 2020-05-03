18317ba1cd41cadf1ba8af761c6baaf8bd19667c
Brad Morgan <brad.morgan@vistarmedia.com>
2020-05-03 08:37:06 -0400

    Added auth0 refresh

--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -4,14 +4,14 @@ module.exports = {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_SCOPE: "openid profile",
+    AUTH0_SCOPE: "openid profile offline_access",
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
     REDIRECT_URI:
       process.env.REDIRECT_URI || "http://localhost:8080/api/callback",
     POST_LOGOUT_REDIRECT_URI:
       process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:8080/",
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
-    SESSION_COOKIE_LIFETIME: 7200, // 2 hours
+    SESSION_COOKIE_LIFETIME: process.env.SESSION_COOKIE_LIFETIME || 7200,
   },
   webpack(config) {
     // This allows us to import "pg" for SSR, but ignore it on the client
